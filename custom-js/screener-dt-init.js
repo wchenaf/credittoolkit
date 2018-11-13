f6f2feb7390b175f66cd0a1d6eece10a5f1d8971
NeilChan <wchenaf@gmail.com>
2018-11-14 04:34:14 +0800

    included new requests package from rmccue

--- custom-js/screener-dt-init.js
+++ custom-js/screener-dt-init.js
@@ -6,85 +6,17 @@
  *     // code here
  * });
  */
-		/* DATA TABLES */
-			
-			function init_DataTables() {
-				
-				if( typeof ($.fn.DataTable) === 'undefined'){ return; }
-				
-				var handleDataTableButtons = function() {
-				  if ($("#datatable-buttons").length) {
-					$("#datatable-buttons").DataTable({
-					  dom: "Bfrtip",
-					  buttons: [
-						{
-						  extend: "copy",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "csv",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "excel",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "pdfHtml5",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "print",
-						  className: "btn-sm"
-						},
-					  ],
-					  responsive: true
-					});
-				  }
-				};
+/* DATA TABLES */
 
-				TableManageButtons = function() {
-				  "use strict";
-				  return {
-					init: function() {
-					  handleDataTableButtons();
-					}
-				  };
-				}();
+function init_DataTables() {
 
-				//$('#datatable').dataTable();
+	if (typeof ($.fn.DataTable) === 'undefined') { return; }
 
-				$('#datatable-keytable').DataTable({
-				  keys: true
-				});
-
-				//$('#resultsTable').DataTable();
-
-				$('#rankTable').DataTable({
-					"paging":false,
-					"ordering": false,
-					"dom" : "<f<t>>",
-					buttons: [
-						{
-							extend: "copy",
-							className: "btn-sm"
-						}
-						],
-				});
-
-				$('#datatable-scroller').DataTable({
-				  ajax: "js/datatables/json/scroller-demo.json",
-				  deferRender: true,
-				  scrollY: 380,
-				  scrollCollapse: true,
-				  scroller: true
-				});
-
-				$('#resultsTable').DataTable({
-					fixedHeader: true,
-					keys: true,
-					"dom" : "<lf<t>Bp>",
-					buttons: [
+	var handleDataTableButtons = function () {
+		if ($("#datatable-buttons").length) {
+			$("#datatable-buttons").DataTable({
+				dom: "Bfrtip",
+				buttons: [
 					{
 						extend: "copy",
 						className: "btn-sm"
@@ -105,31 +37,99 @@
 						extend: "print",
 						className: "btn-sm"
 					},
-					],
-				});
+				],
+				responsive: true
+			});
+		}
+	};
 
-				var $datatable = $('#datatable-checkbox');
+	TableManageButtons = function () {
+		"use strict";
+		return {
+			init: function () {
+				handleDataTableButtons();
+			}
+		};
+	}();
 
-				$datatable.dataTable({
-				  'order': [[ 1, 'asc' ]],
-				  'columnDefs': [
-					{ orderable: false, targets: [0] }
-				  ]
-				});
-				$datatable.on('draw.dt', function() {
-				  $('checkbox input').iCheck({
-					checkboxClass: 'icheckbox_flat-green'
-				  });
-				});
+	//$('#datatable').dataTable();
 
-				TableManageButtons.init();
-				
-			};
-		
-	   
-	$(document).ready(function() {
-		init_DataTables();
-				
-	});	
-	
+	$('#datatable-keytable').DataTable({
+		keys: true
+	});
+
+	//$('#resultsTable').DataTable();
+
+	$('#rankTable').DataTable({
+		"paging": false,
+		"ordering": false,
+		"dom": "<f<t>>",
+		buttons: [
+			{
+				extend: "copy",
+				className: "btn-sm"
+			}
+		],
+	});
+
+	$('#datatable-scroller').DataTable({
+		ajax: "js/datatables/json/scroller-demo.json",
+		deferRender: true,
+		scrollY: 380,
+		scrollCollapse: true,
+		scroller: true
+	});
+
+	$('#resultsTable').DataTable({
+		fixedHeader: true,
+		keys: true,
+		"dom": "<lf<t>Bp>",
+		buttons: [
+			{
+				extend: "copy",
+				className: "btn-sm"
+			},
+			{
+				extend: "csv",
+				className: "btn-sm"
+			},
+			{
+				extend: "excel",
+				className: "btn-sm"
+			},
+			{
+				extend: "pdfHtml5",
+				className: "btn-sm"
+			},
+			{
+				extend: "print",
+				className: "btn-sm"
+			},
+		],
+	});
+
+	var $datatable = $('#datatable-checkbox');
+
+	$datatable.dataTable({
+		'order': [[1, 'asc']],
+		'columnDefs': [
+			{ orderable: false, targets: [0] }
+		]
+	});
+	$datatable.on('draw.dt', function () {
+		$('checkbox input').iCheck({
+			checkboxClass: 'icheckbox_flat-green'
+		});
+	});
+
+	TableManageButtons.init();
+
+};
+
+
+$(document).ready(function () {
+	init_DataTables();
+
+});
+
 
